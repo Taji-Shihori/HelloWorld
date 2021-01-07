cccb8a99e63755570d1b38c421bc5d0496b0be03
root <s-taji@connectcrew.co.jp>
2021-01-07 17:37:56 +0900

    hello world

---
+++ hello-world/src/main/java/DbSample.java
@@ -0,0 +1,58 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DbSample {
+
+  public static void main(String[] args) {
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    try {
+
+      con = DriverManager.getConnection(
+        "jdbc:mysql://localhost/db1?serverTimezone=JST&useSSL=false",
+        "root",
+        "Marika2828"
+      );// "password"の部分は，各自の環境に合わせて変更してください。
+
+      pstmt = con.prepareStatement("select * from tb1 where toshi > 30");
+
+      rs = pstmt.executeQuery();
+
+      while (rs.next()) {
+    	System.out.println(rs.getString("bang") + "," + rs.getString("nama") + "," + rs.getInt("toshi"));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+	} finally {
+      if (rs != null) {
+        try {
+          rs.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+      if (pstmt != null) {
+        try {
+          pstmt.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+      if (con != null) {
+        try {
+          con.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
